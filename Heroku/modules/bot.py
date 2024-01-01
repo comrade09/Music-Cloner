7e25310693395d22c7f04fbb04e4ff6100b5bf8d
Comrade X <106185020+comrade09@users.noreply.github.com>
2024-01-01 11:27:12 +0530

    Create bot.py

---
+++ Heroku/modules/bot.py
@@ -0,0 +1,45 @@
+import asyncio
+
+from pyrogram import Client, filters, __version__ as pyrover
+from pyrogram.errors import FloodWait, UserNotParticipant
+from pytgcalls import (__version__ as pytover)
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ChatJoinRequest
+
+from time import time
+from datetime import datetime
+
+from Heroku.setup.filters import command
+from Heroku.config import BOT_NAME, OWNER_USERNAME, UPDATE, SUPPORT, BOT_USERNAME
+
+
+
+HELP_TEXT = """
+ʜᴇʏᴀ! {}
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+✘ ɪ'ᴍ ᴊᴜꜱᴛ ɴᴏᴛ ᴀ ᴍᴜꜱɪᴄ ʙᴏᴛ ɪ ʜᴀᴠᴇ ʟᴏᴛꜱ ᴏꜰ ꜰᴇᴀᴛᴜʀᴇꜱ ᴡʜɪᴄʜ ʏᴏᴜ ʟɪᴋᴇꜱ ᴛʜᴀᴛ.
+‣ ɪ ᴄᴀɴ ᴘʟᴀʏ ᴀᴜᴅɪᴏ+ᴠɪᴅᴇᴏ ʙᴏᴛʜ.
+‣ ɪ ʜᴀᴠᴇ ᴀʟᴍᴏꜱᴛ ᴀʟʟ ꜰᴇᴀᴛᴜʀᴇꜱ ᴡʜɪᴄʜ ɴᴇᴇᴅꜱ ᴀ ᴍᴜꜱɪᴄ ʙᴏᴛ.
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+✘ ᴄʟɪᴄᴋ ᴏɴ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ 🔘 ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ ℹ️.
+"""
+
+
+@Client.on_message(command("/bot") & filters.private & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_text(f"{HELP_TEXT}".format(message.from_user.mention()),
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "✚ ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ᴀᴅᴅ ᴍᴇ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+                ],
+                [
+                    InlineKeyboardButton(
+                        "📡 ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{UPDATE}"),
+                    InlineKeyboardButton(
+                        "☁️ ᴏᴛʜᴇʀs", callback_data="others")
+                ]
+           ]
+        ),
+    )
+
